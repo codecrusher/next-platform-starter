98d350dfbab20d074228ec311671122e3a67a097
aksje-utv <aksje-utv@nothing.com>
2024-10-20 00:34:25 +0200

    Header fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import { Header } from '../components/header';
 
 export const metadata = {
     title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
+        template: '%s | HeritageTree',
+        default: 'HeritageTree'
     }
 };
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-slate-400">
+            <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
