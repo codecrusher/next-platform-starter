6e773908656a7a4d86eb471d58a40c52db88e5b2
aksje-utv <aksje-utv@nothing.com>
2024-10-20 00:18:28 +0200

    Try some basic editing

--- app/page.jsx
+++ app/page.jsx
@@ -32,13 +32,13 @@ export default function Page() {
         <main className="flex flex-col gap-8 sm:gap-16">
             <section className="flex flex-col items-start gap-3 sm:gap-4">
                 <ContextAlert />
-                <h1 className="mb-0">Netlify Platform Starter - Next.js</h1>
-                <p className="text-lg">Get started with Next.js and Netlify in seconds.</p>
+                <h1 className="mb-0">HeritageTree &emdash; Create your own heritage tree!</h1>
+                <p className="text-lg">Just drag and drop nodes and get started</p>
                 <Link
-                    href="https://docs.netlify.com/frameworks/next-js/overview/"
+                    href="/"
                     className="btn btn-lg btn-primary sm:btn-wide"
                 >
-                    Read the Docs
+                    Get started
                 </Link>
             </section>
             {!!ctx && (
